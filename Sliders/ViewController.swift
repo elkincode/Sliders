86a37f50368c9084ea786d4f98fa60e305c1b6bf
Ivan Elkin <elk.iv@yandex.ru>
2021-08-23 15:56:16 +0300

    improved code readability

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    
     @IBOutlet weak var redValueLabel: UILabel!
     @IBOutlet weak var greenValueLabel: UILabel!
     @IBOutlet weak var blueValueLabel: UILabel!
@@ -40,7 +41,10 @@ class ViewController: UIViewController {
         let greenSliderValue = CGFloat(greenSlider.value)
         let blueSliderValue = CGFloat(blueSlider.value)
         
-        colorDisplayView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+        colorDisplayView.backgroundColor = UIColor(red: redSliderValue,
+                                                   green: greenSliderValue,
+                                                   blue: blueSliderValue,
+                                                   alpha: 1)
         
         typeOfLabel.text = String(format: "%.2f", typeOfSlider.value)
     }
